1a8d004b5bcdb2c3709d715c6a924c70ca3ad5bd
LYTOOS <103782894+LYTOOS@users.noreply.github.com>
2026-01-18 21:36:28 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getFirestore, addDoc, collection } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { serverTimestamp } from
+"https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
 apiKey: "AIzaSyCZwMV1DuW6W-QAU3ImqQ_vCoyHB-W-aEo",
@@ -13,6 +15,7 @@ const db = getFirestore(app);
 window.saveRSVP=(name)=>{
 addDoc(collection(db,"rsvp"),{
 nama:name,
-tanggal:new Date()
+createdAt: serverTimestamp(),
+userAgent: navigator.userAgent
 });
 }
